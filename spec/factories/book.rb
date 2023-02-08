defa75f3c2339c2c3af154b33ff952a4b98ef7ca
鎌田裕也 <yk0417yuya@gmail.com>
2023-02-08 12:29:12 +0900

    RuboCopの修正

--- spec/factories/book.rb
+++ spec/factories/book.rb
@@ -7,7 +7,7 @@
 
 FactoryBot.define do
   factory :book do
-    title { Faker::Lorem.characters(number:5) }
-    body { Faker::Lorem.characters(number:20) }
+    title { Faker::Lorem.characters(number: 5) }
+    body { Faker::Lorem.characters(number: 20) }
   end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,9 +1,9 @@
 # This file is copied to spec/ when you run 'rails generate rspec:install'
 require 'spec_helper'
 ENV['RAILS_ENV'] ||= 'test'
-require File.expand_path('../../config/environment', __FILE__)
+require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
-abort("The Rails environment is running in production mode!") if Rails.env.production?
+abort('The Rails environment is running in production mode!') if Rails.env.production?
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 # Webdrivers.logger.level = ::Logger::Severity::DEBUG
@@ -30,11 +30,11 @@ rescue ActiveRecord::PendingMigrationError => e
   puts e.to_s.strip
   exit 1
 end
-#Rails.logger = Logger.new(STDOUT)
-#ActiveRecord::Base.logger = Logger.new(STDOUT)
+# Rails.logger = Logger.new(STDOUT)
+# ActiveRecord::Base.logger = Logger.new(STDOUT)
 RSpec.configure do |config|
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
-  config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.fixture_path = "#{Rails.root}/spec/fixtures"
 
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
@@ -60,5 +60,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
-	config.include FactoryBot::Syntax::Methods
+  config.include FactoryBot::Syntax::Methods
 end

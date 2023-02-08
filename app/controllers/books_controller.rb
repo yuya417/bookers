6831796be81445d48dfbe50eb5488b6c18564693
鎌田裕也 <yk0417yuya@gmail.com>
2023-02-08 12:08:29 +0900

    RuboCopの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,18 +4,6 @@ class BooksController < ApplicationController
     @books = Book.all
   end
 
-  def create
-    @book = Book.new(book_params)
-    @books = Book.all
-
-    if @book.save
-      flash[:notice] = "Book was successfully created."
-      redirect_to book_path(@book.id)
-    else
-      render :index
-    end
-  end
-
   def show
     @book = Book.find(params[:id])
   end
@@ -24,15 +12,27 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def create
+    @book = Book.new(book_params)
+    @books = Book.all
+
+    if @book.save
+      flash[:notice] = 'Book was successfully created.'
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+  end
+
   def update
     @book = Book.find(params[:id])
 
     if @book.update(book_params)
-      flash[:notice] = "Book was successfully updated."
+      flash[:notice] = 'Book was successfully updated.'
       redirect_to book_path(@book.id)
     else
       render :edit
-    end 
+    end
   end
 
   def destroy
